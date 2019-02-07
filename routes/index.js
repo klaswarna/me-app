97a432c9fa4104e7d55e8a3404f3cf3930229180
klaswarna <klas.warna@gmail.com>
2019-02-07 12:57:31 +0100

    minor text changes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.get('/', function(req, res, next) {
     const data = {
         data: {
             msg: [
-                "Hej. Jag heter Klas W. Detta är min sista dbwebb-kurs. Det känns lite sorgligt känns det för de har varit mycket givande, men det känns desto roligare att snart vara klar med hela utbildningen",
+                "Hej. Jag heter Klas W. Detta är min sista dbwebb-kurs. Lite sorgligt känns det för det har varit mycket givande, men det känns desto roligare att snart vara klar med hela utbildningen",
                 "För varje ny sak man lär sig stöter man på tio saker som är okända, därför känns det stundom som att man inget kan i den stora programmeringsvärlden, men när jag tittar tillbaka på vad jag kunde för ett och ett halvt år sedan inser jag att jag lärt mig massor.",
                 "Jag har har de kunskaper som krävs för att skapa en godtycklig webbsida med interaktivt innehåll (givet vissa restriktioner), eller kanske en app till mobilen på samma vis. Och den kunskapen är värd en hel del. Nästa steg är förstås att göra det snabbt och snyggt också, kanske helst på ett dussin olika sätt, men det får man ta allt eftersom."
             ]
